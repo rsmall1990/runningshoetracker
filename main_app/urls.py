0c6aa2b9ea59386e52b39f176e7bc905afbbaea6
Ryan Small <ryan.small@feapc.com>
2022-03-16 20:11:47 -0400

    wip: adding index

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
 
-urlpatters = [
-    path('', views.home, name='home')
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('shoes/', views.shoes_index, name='index'),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+"""
+Fake data
+"""
+
+class shoe:
+    def __init__(self, brand, color, miles):
+        self.brand = brand
+        self.color = color
+        self.miles = miles
+
+shoes = [
+    shoe('nikes', 'black', 200),
+    shoe('brooks', 'orange', 150)
+]
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html', )
+
+def about(request):
+    return render(request, 'about.html', )
+
+def shoes_index(request):
+    return render(request, 'shoes/index.html', {'shoes': shoes})
