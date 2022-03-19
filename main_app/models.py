3f71956faa86d917510b0b84a6c6a023b0854d66
Ryan Small <ryan.small@feapc.com>
2022-03-19 12:31:56 -0400

    wip: updating models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+Runs = (
+    ('L', 'Long'),
+    ('S', 'Speed'),
+    ('E', 'Easy'),
+)
+
 class Shoe(models.Model):
     brand = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
@@ -11,3 +17,11 @@ class Shoe(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'shoe_id': self.id})
+
+class Run(models.Model):
+    date = models.DateField()
+    run = models.CharField(max_length=1, choices=Runs, default=[2][0])
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_run_display()} run on {self.date}"
